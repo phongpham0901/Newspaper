d2076c4758bc15e27616e2e3892fcec6ac7e6d0a
Phạm Xuân Phong <phamxuanphong09012003@gmail.com>
2024-05-30 09:58:22 +0700

    Update 1

--- WebApplication1/WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/WebApplication1/App_Start/RouteConfig.cs
@@ -14,6 +14,14 @@ namespace WebApplication1
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+           "Employee", "Employee/{name}", new
+           {
+               controller = "Employee",
+               action = "Search",
+               name = UrlParameter.Optional
+           });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
